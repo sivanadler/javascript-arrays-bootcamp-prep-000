88ad059bcf974c14909f28bf852f4f9bcc08d21a
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-03 01:20:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ var accessElementInArray = function (array, index){
 }
 
 var destructivelyRemoveElementFromBeginningOfArray = function (array){
-  destructivelyRemoveElementFromBeginningOfArray = [1, 2, 3]
+  destructivelyRemoveElementFromBeginningOfArray = [ 2, 3]
   array.unshift()
   return destructivelyRemoveElementFromBeginningOfArray
 }
